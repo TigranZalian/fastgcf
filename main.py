fabe514fd07d74d5fa492b03bbeebe9aa3c6ab89
Tigran Zalian <tigraanzalian@gmail.com>
2023-09-23 22:12:44 +0200

    Revert "patch: performance improvements"
    This reverts commit c1f51f0217f6d0c409472b0008199f74516ca169.

--- main.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from datetime import date
-from fastgcf import router
-
-# Simply use a decorator
-@router.get()
-async def main(start_date: date, end_date: date):
-    return {"start_date": start_date, "end_date": end_date}
